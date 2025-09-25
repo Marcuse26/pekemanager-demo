7b0808f92c4e80dca7139585c4ea2ad5a59e8377
Marcuse26 <marcos.serranomartinez.alumnos@gmail.com>
2025-09-25 19:25:42 +0200

    1.1

--- src/config/schedules.ts
+++ src/config/schedules.ts
@@ -2,19 +2,11 @@
 import type { Schedule } from '../types';
 
 export const schedules: Schedule[] = [
-    { id: 'h_305', name: 'Cuota 305€', price: 305, endTime: '12:00' },
     { id: 'h_315', name: 'Cuota 315€ (8:30-12:30)', price: 315, endTime: '12:30' },
-    { id: 'h_400', name: 'Cuota 400€', price: 400, endTime: '13:00' },
     { id: 'h_410', name: 'Cuota 410€ (8:30-13:30)', price: 410, endTime: '13:30' },
-    { id: 'h_415', name: 'Cuota 415€', price: 415, endTime: '13:00' },
     { id: 'h_425', name: 'Cuota 425€ (8:30-15:00)', price: 425, endTime: '15:00' },
-    { id: 'h_440', name: 'Cuota 440€', price: 440, endTime: '15:00' },
     { id: 'h_450', name: 'Cuota 450€ (8:30-15:30)', price: 450, endTime: '15:30' },
     { id: 'h_460', name: 'Cuota 460€ (8:30-16:30)', price: 460, endTime: '16:30' },
-    { id: 'h_480', name: 'Cuota 480€', price: 480, endTime: '17:00' },
     { id: 'h_495', name: 'Cuota 495€ (8:30-17:00)', price: 495, endTime: '17:00' },
-    { id: 'h_510', name: 'Cuota 510€', price: 510, endTime: '17:30' },
-    { id: 'h_530', name: 'Cuota 530€', price: 530, endTime: '18:00' },
     { id: 'h_545', name: 'Cuota 545€ (8:30-18:00)', price: 545, endTime: '18:00' },
-    { id: 'h_560', name: 'Cuota 560€', price: 560, endTime: '18:30' },
 ];
